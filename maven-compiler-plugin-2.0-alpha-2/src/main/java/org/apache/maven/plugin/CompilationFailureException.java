3d7a6d1ad429f817296a5ca04898fd5dbe2169cc
Brett Leslie Porter <brett@apache.org>
2005-05-13 08:25:16 +0000

    tag plugin release
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/tags/maven-plugins@169978 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-compiler-plugin-2.0-alpha-2/src/main/java/org/apache/maven/plugin/CompilationFailureException.java
@@ -0,0 +1,36 @@
+package org.apache.maven.plugin;
+
+import org.codehaus.plexus.compiler.CompilerError;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
+ * @version $Id$
+ */
+public class CompilationFailureException
+    extends MojoExecutionException
+{
+    private static final String LS = System.getProperty( "line.separator" );
+
+    public CompilationFailureException( List messages )
+    {
+        // TODO: this is a bit nasty
+        super( messages, "Compilation failure", longMessage( messages ) );
+    }
+
+    public static String longMessage( List messages )
+    {
+        StringBuffer sb = new StringBuffer();
+
+        for ( Iterator it = messages.iterator(); it.hasNext() ; )
+        {
+            CompilerError compilerError = (CompilerError) it.next();
+
+            sb.append( compilerError ).append( LS );
+        }
+
+        return sb.toString();
+    }
+}
---
+++ maven-compiler-plugin-2.0-alpha-2/src/main/java/org/apache/maven/plugin/TestCompilerMojo.java
@@ -0,0 +1,67 @@
+package org.apache.maven.plugin;
+
+import java.util.List;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
+ * @version $Id$
+ * @goal testCompile
+ * @description Compiles test sources
+ * @requiresDependencyResolution test
+ */
+public class TestCompilerMojo
+    extends AbstractCompilerMojo
+{
+    /**
+     * @parameter expression="${project.testCompileSourceRoots}"
+     * @required
+     * @readonly
+     */
+    private List compileSourceRoots;
+
+    /**
+     * @parameter expression="${project.testClasspathElements}"
+     * @required
+     * @readonly
+     */
+    private List classpathElements;
+
+    /**
+     * @parameter expression="${project.build.testOutputDirectory}"
+     * @required
+     * @readonly
+     */
+    private String outputDirectory;
+
+    protected List getCompileSourceRoots()
+    {
+        return compileSourceRoots;
+    }
+
+    protected List getClasspathElements()
+    {
+        return classpathElements;
+    }
+
+    protected String getOutputDirectory()
+    {
+        return outputDirectory;
+    }
+
+}
