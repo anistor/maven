dd112bf3d7ed7aa544918d4a8bf38daa75f193b7
Britton Isbell <sisbell@apache.org>
2008-10-27 07:53:31 +0000

    Implementations for the active profile matchers.
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/branches/sisbell-maven-3.0-mercury@708102 13f79535-47bb-0310-9956-ffa450edef68

--- maven-project-builder/src/main/java/org/apache/maven/project/builder/profile/ActiveProfileMatcher.java
+++ maven-project-builder/src/main/java/org/apache/maven/project/builder/profile/ActiveProfileMatcher.java
@@ -6,6 +6,13 @@ import org.apache.maven.shared.model.InterpolatorProperty;
 import java.util.List;
 
 public interface ActiveProfileMatcher {
-    
+
+    /**
+     * If model container does not contain the activator property, must return false.
+     *
+     * @param modelContainer
+     * @param properties
+     * @return
+     */
     boolean isMatch(ModelContainer modelContainer, List<InterpolatorProperty> properties);
 }
--- maven-project-builder/src/main/java/org/apache/maven/project/builder/profile/OperatingSystemMatcher.java
+++ maven-project-builder/src/main/java/org/apache/maven/project/builder/profile/OperatingSystemMatcher.java
@@ -2,12 +2,41 @@ package org.apache.maven.project.builder.profile;
 
 import org.apache.maven.shared.model.ModelContainer;
 import org.apache.maven.shared.model.InterpolatorProperty;
+import org.apache.maven.shared.model.ModelProperty;
+import org.apache.maven.project.builder.ProjectUri;
 
 import java.util.List;
 
 public class OperatingSystemMatcher implements ActiveProfileMatcher {
 
     public boolean isMatch(ModelContainer modelContainer, List<InterpolatorProperty> properties) {
+        if(modelContainer == null ) {
+            throw new IllegalArgumentException("modelContainer: null");
+        }
+
+        String operatingSystem = null, version = null, arch = null, family = null;
+
+        for(InterpolatorProperty property : properties) {
+            if(property.getKey().equals("${os.name}")) {
+                operatingSystem = property.getValue();
+            } else if(property.getKey().equals("${os.version}")) {
+               version = property.getValue();
+            } else if(property.getKey().equals("${os.arch}")) {
+                arch = property.getValue();
+            } else if(property.getKey().equals("${os.family}")) {
+                family = property.getValue();
+            }
+        }
+
+        boolean matchesOs, matchesVersion, matchesArch, matchesFamily;
+
+        
+        for(ModelProperty property : modelContainer.getProperties()) {
+            if(arch !=null && property.getUri().equals(ProjectUri.Profiles.Profile.Activation.Os.arch)) {
+                
+            }
+        }
+
         return false;
     }
 }
