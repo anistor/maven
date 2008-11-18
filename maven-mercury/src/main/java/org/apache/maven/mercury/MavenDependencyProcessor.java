882f976658991e608b33ea9e40039d8edcd00a0f
Britton Isbell <sisbell@apache.org>
2008-11-18 17:29:00 +0000

    Add in implicit maven version property.
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/branches/sisbell-maven-3.0-mercury@718660 13f79535-47bb-0310-9956-ffa450edef68

--- maven-mercury/src/main/java/org/apache/maven/mercury/MavenDependencyProcessor.java
+++ maven-mercury/src/main/java/org/apache/maven/mercury/MavenDependencyProcessor.java
@@ -29,6 +29,9 @@ public final class MavenDependencyProcessor implements DependencyProcessor {
         }
 
         List<InterpolatorProperty> interpolatorProperties = new ArrayList<InterpolatorProperty>();
+        interpolatorProperties.add(new InterpolatorProperty("${mavenVersion}", "3.0-SNAPSHOT", PomInterpolatorTag.SYSTEM_PROPERTIES.name()));
+        interpolatorProperties.add(new InterpolatorProperty("${maven.version}", "3.0-SNAPSHOT", PomInterpolatorTag.SYSTEM_PROPERTIES.name()));
+        
         if(system != null) {
             interpolatorProperties.addAll( InterpolatorProperty.toInterpolatorProperties( system,
                     PomInterpolatorTag.SYSTEM_PROPERTIES.name()));
